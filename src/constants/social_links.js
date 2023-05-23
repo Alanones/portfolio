f438f2acd600b20b8b263ed52f7caac7fd0b097d
Alanones <109990442+Alanones@users.noreply.github.com>
2023-05-23 13:52:13 +0300

    Update social_links.js

--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -11,22 +11,22 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/alan-maluki-4330b1264/",
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://github.com/Alanones",
   },
   {
     id: 4,
     icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.behance.com",
   },
   {
     id: 5,
